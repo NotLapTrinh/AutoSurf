c742cf0ca334aa23d3ec786ad03444759333da41
Luong Cuong <luongcuong1991@gmail.com>
2017-06-24 19:36:48 +0700

    optionghichu

--- notlaptrinh-content.js
+++ notlaptrinh-content.js
@@ -1,15 +1,21 @@
-var urls = [{
-	u: '/',
-	t: 'Nốt Lập Trình'
-}, {
-	u: '/simple-auto-surf',
-	t: 'Chương trình auto surf đơn giản - Nốt Lập Trình'
-}];
 
-var url = urls[parseInt(Math.random() * urls.length)];
+var flag = false; //Muốn sửa lại tiêu đề, và url trên trình duyệt thì chuyển true.
 
-document.title = url.t;
-window.history.pushState(null, null, url.u);
+if(flag){
+	var urls = [{
+		u: '/',
+		t: 'Nốt Lập Trình'
+	}, {
+		u: '/simple-auto-surf',
+		t: 'Chương trình auto surf đơn giản - Nốt Lập Trình'
+	}];
+	
+	var url = urls[parseInt(Math.random() * urls.length)];
+
+	//fake tiêu đề, url
+	document.title = url.t;
+	window.history.pushState(null, null, url.u);
+}
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
